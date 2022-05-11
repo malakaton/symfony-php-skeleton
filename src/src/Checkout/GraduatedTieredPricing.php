af670b99a7ac4aaf21f808aa0bd540441673f5fc
Manel Bermúdez <manel.bermudez@deporvillage.com>
2022-05-11 11:36:30 +0200

    [tpp] 🟢 6. unconditional -> if

--- src/src/Checkout/GraduatedTieredPricing.php
+++ src/src/Checkout/GraduatedTieredPricing.php
@@ -8,6 +8,10 @@ final class GraduatedTieredPricing
 {
     public function total(int $subscriptions): int
     {
+        if ($subscriptions === 3) {
+            return 837;
+        }
+
         return $subscriptions * 299;
     }
 }
