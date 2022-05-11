f0334c190952e41e8bc24c9421f254d62510f6b2
Manel Bermúdez <manel.bermudez@deporvillage.com>
2022-05-11 11:04:14 +0200

    [tpp] 🟢 compile works

---
+++ src/tests/Unit/Checkout/GraduatedTieredPricingTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Checkout;
+
+use App\Tests\Unit\Shared\Infrastructure\PhpUnit\UnitTestCase;
+
+class GraduatedTieredPricingTest extends UnitTestCase
+{
+    /**
+     * @test
+     */
+    public function it_should_works_graduated_tiered_pricing(): void
+    {
+        self::assertTrue(true);
+    }
+}
