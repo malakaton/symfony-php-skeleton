435797fac517be225694f12ed42b18ec10610e1a
Manel Bermúdez <manel.bermudez@deporvillage.com>
2022-05-11 11:40:59 +0200

    [tpp] 🔵 decompose constants

--- src/src/Checkout/GraduatedTieredPricing.php
+++ src/src/Checkout/GraduatedTieredPricing.php
@@ -9,11 +9,11 @@ final class GraduatedTieredPricing
     public function total(int $subscriptions): int
     {
         if ($subscriptions === 4) {
-            return 1076;
+            return 299 + 299 + 239 + 239;
         }
 
         if ($subscriptions === 3) {
-            return 837;
+            return 299 + 299 + 239;
         }
 
         return $subscriptions * 299;
